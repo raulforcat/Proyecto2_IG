8d1ca223b75db2129b51bed4ac6cbd78a34004a3
raulforcat <raulforcat@gmail.com>
2026-06-17 19:09:14 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/estado', (req, res) => {
-    res.json({ status: 'Servidor en funcionamiento' });
+    res.json({ status: "Servidor en funcionamiento" });
+});
+
+app.get("/puerta", (req, res) => {
+    res.json({
+        estado: "abierta"
+    });
 });
 
 const PORT = process.env.PORT || 3000;
