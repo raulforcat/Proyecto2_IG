1eb3f9b8358ca750888f8a0f43a24ea883e8ede5
raulforcat <raulforcat@gmail.com>
2026-06-17 19:16:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/puerta", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`);
